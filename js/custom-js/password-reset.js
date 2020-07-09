3e4d0834c1bb4edb6183d023b2c5046b2c3da633
sccola <sccola2020@gmail.com>
2020-07-09 18:51:38 +0100

    feat: reset password api implementation

--- js/custom-js/password-reset.js
+++ js/custom-js/password-reset.js
@@ -15,27 +15,59 @@ resetPasswordBtn.addEventListener('click', (e) => {
         resetToken = param[0];
     }
     //return token;
+    
     console.log(resetToken);
-    fetch(`https://api.lancers.app/v1/auth/reset-password/${resetToken}`, {
-        method: "PUT",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8",
-            "User-Agent": "Developers Lancers",
-        },
-        // body: JSON.stringify({email})
-        body: JSON.stringify(newPassword)
-    })
-        .then( res => res.json() )
-        // .then(data => console.log(data))
-        .then((data) => {
-            // console.log(data);
-            if (data.status === 'success') {
-                window.location = 'password-success.html';
-            //   $('#exampleModal').modal();
-              // document.querySelector('.loaderV2').style.display = 'none'
-            // } else {
-            //   showAlert(data.error)
-            }
-          })
-        .catch(err => console.log(err));
+    // fetch(`https://api.lancers.app/v1/auth/reset-password/${resetToken}`, {
+    //     method: "PUT",
+    //     headers: {
+    //         "Content-type": "application/json; charset=UTF-8",
+    //         "User-Agent": "Developers Lancers",
+    //     },
+    //     // body: JSON.stringify({email})
+    //     body: JSON.stringify(newPassword)
+    // })
+    //     .then( res => res.json() )
+    //     // .then(data => console.log(data))
+    //     .then((data) => {
+    //         // console.log(data);
+    //         if (data.status === 'success') {
+    //             window.location = 'password-success.html';
+    //         //   $('#exampleModal').modal();
+    //           // document.querySelector('.loaderV2').style.display = 'none'
+    //         // } else {
+    //         //   showAlert(data.error)
+    //         }
+    //       })
+    //     .catch(err => console.log(err));
+
+    axios({
+        method: 'PUT',
+        url: `https://api.lancers.app/v1/auth/reset-password/${resetToken}`,
+        data: {
+          password: newPassword
+        }
+      })
+      .then((res) => {
+        submitButton.innerText = 'Login'
+        if(res.data.status === 'success') {        
+            console.log(res.data);
+            // console.log(res.data.data.token);
+            // console.log(res.data.data.user);
+        //     localStorage.setItem('tpAuth', JSON.stringify({
+        //       token: res.data.data.token,
+        //       userId: res.data.data.user
+        //    }));
+    
+          return (window.location.href = "/password-success");
+    
+        } else {
+        //   submitButton.innerText = 'Login';
+          showAlert(res.error)
+        }
+      })
+      .catch((err) => {
+            //  showAlert(err.message)
+            console.log('Error', err.message)
+      })
 })
+
